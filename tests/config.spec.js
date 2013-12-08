0df9f2c8305a6f9cf9a13114568952ed84e58a99
Ian Glazer <ian.glazer@gmail.com>
2013-12-08 17:27:29 -0500

    got db

--- tests/config.spec.js
+++ tests/config.spec.js
@@ -2,16 +2,22 @@ describe("Configuration setup", function() {
     it("should load local configurations", function(next) {
         var config = require('../config')();
         expect(config.mode).toBe('local');
+        expect(config.mongo.host).toBe('127.0.0.1');
+        expect(config.mongo.port).toBe(27017);
         next();
     });
     it("should load staging configurations", function(next) {
         var config = require('../config')('staging');
         expect(config.mode).toBe('staging');
+        expect(config.mongo.host).toBe('127.0.0.1');
+        expect(config.mongo.port).toBe(27017);
         next();
     });
     it("should load production configurations", function(next) {
         var config = require('../config')('production');
         expect(config.mode).toBe('production');
+        expect(config.mongo.host).toBe('127.0.0.1');
+        expect(config.mongo.port).toBe(27017);
         next();
     });
 });
